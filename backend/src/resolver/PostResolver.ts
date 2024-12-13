32a96ef6179083356149a54488c7dea53e93d56d
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2024-12-13 15:19:03 +0100

    unaccent search

--- backend/src/resolver/PostResolver.ts
+++ backend/src/resolver/PostResolver.ts
@@ -1,3 +1,4 @@
+import { dataSource } from "../config/db";
 import Post from "../entity/Post";
 import { Arg, Field, InputType, Mutation, Query, Resolver } from "type-graphql";
 
@@ -10,8 +11,10 @@ class NewPostInput implements Partial<Post> {
 @Resolver(Post)
 class PostResolver {
   @Query(() => [Post])
-  async getAllPosts() {
-    return await Post.find();
+  async searchPosts(@Arg("keyword") keyword: String) {
+    const sql = `SELECT * FROM post WHERE unaccent(post.content) ILIKE unaccent($1)`;
+    const result = await dataSource.query(sql, [`%${keyword}%`]);
+    return result;
   }
 
   @Mutation(() => String)
